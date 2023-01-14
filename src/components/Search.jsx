674666d63f71d06e4c00ea2556bad7afc04ff215
SivaSurender <e.surender14@gmail.com>
2023-01-14 22:33:45 +0530

    Added files

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Search() {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="search-form">
+        <input type="text" placeholder="Find your buddy " />
+      </div>
+      <div className="user-chat">
+        <img
+          src="https://images.pexels.com/photos/1374510/pexels-photo-1374510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="user-chat-info">
+          <span>Test two</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Search;
