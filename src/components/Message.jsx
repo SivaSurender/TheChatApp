d1136ffd4a13f3b36fbd6199988570c903178e7e
SivaSurender <e.surender14@gmail.com>
2023-01-15 15:00:48 +0530

    Added Files

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Message() {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="message-info">
         <img
           src="https://images.pexels.com/photos/15031643/pexels-photo-15031643.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
@@ -13,7 +13,7 @@ function Message() {
       <div className="message-content">
         <p>hello </p>
         <img
-          src="https://images.pexels.com/photos/15052341/pexels-photo-15052341.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+          src="https://images.pexels.com/photos/14189121/pexels-photo-14189121.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
           alt="Receiver Photo"
         />
       </div>
